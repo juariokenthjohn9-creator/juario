d7a007207099995b317fcad119afee07cd35dd62
juariokenth9-creatot <juariokenthjohn9@gmail.com>
2025-07-24 01:50:47 -0700

    hahaha

---
+++ src/juario/Juario.java
@@ -0,0 +1,30 @@
+
+package juario;
+
+import java.util.Scanner;
+
+
+public class Juario {
+
+   
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int age;
+        
+        System.out.print("Enter your name: ");
+        name = sc.nextLine()
+        System.out.print("Enter your age: ");
+        age = sc.nextInt();
+        
+        System.out.println("/n\Hello "+name+" your age is"+age);
+        
+        if(age >= 18){
+            System.out.println("Votable");
+        }else{
+            System.out.println("Minority");
+        }
+    }
+    
+}
