7792ca47af90fe444cd9277eb6b64289c9274e3b
SupaMiku <leudisraelquintana@gmail.com>
2025-08-04 18:36:27 -0700

    3rd push

--- src/juario/Juario.java
+++ src/juario/Juario.java
@@ -9,21 +9,7 @@ public class Juario {
    
      
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String name;
-        int age;
-        
-        System.out.print("Enter your name: ");
-        name = sc.nextLine();
-        System.out.print("Enter your age: ");
-        age = sc.nextInt();
-        
-        System.out.println("\nHello "+name+" Your age "+age);
-        
-        if(age >= 18){
-            System.out.println("Votable");
-        }else{
-            System.out.println("Minority");
+      
         }
     }
     
